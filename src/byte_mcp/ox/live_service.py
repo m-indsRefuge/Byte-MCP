7387589deb72036e8a09f4c86a677d80874de463
Nolan Augustine <146803501+m-indsRefuge@users.noreply.github.com>
2026-08-30 08:10:10 +0200

    feat: compose bounded OX audit reliability

--- src/byte_mcp/ox/live_service.py
+++ src/byte_mcp/ox/live_service.py
@@ -1,10 +1,12 @@
 from .approval_replay import InitialApprovalReplayMixin
+from .audit_reliability import SafeAttemptAuditMixin
 from .natural_service import OXReviewService as NaturalOXReviewService
 from .provider_reliability import ProviderReliabilityMixin
 
 
 class OXReviewService(
     InitialApprovalReplayMixin,
+    SafeAttemptAuditMixin,
     ProviderReliabilityMixin,
     NaturalOXReviewService,
 ):
