8289f66dd67aa4ad941918da27affab9c0c3a90f
Nolan Augustine <146803501+m-indsRefuge@users.noreply.github.com>
2026-09-17 21:17:26 +0200

    lint: regroup Task 8 client imports

--- tests/ox/test_client.py
+++ tests/ox/test_client.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 import asyncio
 import json
 
-import byte_mcp.ox.client as ox_client
 import httpx
 import pytest
-from byte_mcp.ox.client import execute_ox_transport, extract_ox_review_text
 
+import byte_mcp.ox.client as ox_client
 from byte_mcp.errors import OXProtocolError
+from byte_mcp.ox.client import execute_ox_transport, extract_ox_review_text
 from byte_mcp.ox.packet import prepare_ox_request
 from byte_mcp.providers import (
     ProviderAttemptOutcome,
